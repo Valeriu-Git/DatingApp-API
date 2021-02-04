e53ad603328f591cc8888aa40bdffc7ec01d09ee
Valeriu <Gabriel.Paduraru@filed.com>
2021-02-04 19:26:49 +0200

    Error handling via middleware

---
+++ CSharpNewAPI/Controllers/BuggyController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using CSharpNewAPI.Models;
+using CSharpNewAPI.Database;
+
+namespace CSharpNewAPI.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        public DatabaseContext DatabaseContext { get; }
+        public BuggyController(DatabaseContext context)
+        {
+            this.DatabaseContext = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuthError()
+        {
+            return "authentication error";
+        }
+
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFoundError()
+        {
+            AppUser user = DatabaseContext.Find<AppUser>(-1);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            Console.WriteLine("test");
+            return user;
+        }
+
+        [HttpGet("null-exception")]
+        public ActionResult<string> GetNullReferenceException()
+        {
+            AppUser user = DatabaseContext.Find<AppUser>(-1);
+            return user.ToString();   
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest();
+        }
+    }
+}
